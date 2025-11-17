cf23ff5958c6b06bc625b3a08175173e8d9eec32
Jerrwin <jermansviniston@gmail.com>
2025-11-18 02:37:56 +0530

    create get API

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -4,16 +4,39 @@ import noteModel from "../models/noteModel.js";
 export const createNote = async (req, res) => {
   try {
     const noteData = new noteModel(req.body);
-    const {title, content} = noteData;
+    const { title, content } = noteData;
 
-    const noteExist = await noteModel.findOne({title, content});
-    if (noteExist){
-        return res.status(400).json({message: "Note with same title and content already exists."});
+    const noteExist = await noteModel.findOne({ title, content });
+    if (noteExist) {
+      return res
+        .status(400)
+        .json({ message: "Note with same title and content already exists." });
     }
     const savedNote = await noteData.save();
-    res.status(200).json({savedNote});
+    res.status(200).json({ savedNote });
   } catch (error) {
     res.status(500).json({ error: error });
   }
 };
 
+//getNotes using GET
+export const getNotes = async (req, res) => {
+  try {
+    const notes = await noteModel.find();
+    if (notes.length === 0) {
+      return res.status(400).json({ message: "No notes stored." });
+    }
+
+    const formattedNotes = notes.map((note) => ({
+      id: note._id,
+      title: note.title,
+      content: note.content,
+      tags: note.tags.join(", "),
+      createdAt: note.createdAt.toLocaleString(),
+    }));
+
+    res.status(200).json({ notes: formattedNotes });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createNote } from "../controllers/noteController.js";
+import { createNote,getNotes } from "../controllers/noteController.js";
 
 const route = express.Router();
 
 route.post("/", createNote);
+route.get("/", getNotes);
 
 export default route;
